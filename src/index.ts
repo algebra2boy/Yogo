fbc915eb6fa9353ec0f9e7de7f1ab1a2dd50b06e
Yongye Tan <yongyetan1209@gmail.com>
2024-04-14 04:51:28 +0000

    fix set up, ready to code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+#!/usr/bin/env node
+
 import { program } from "commander";
 
 program
-  .version("0.1.0")
-  .description("Yogo is a command line tool for generating daily life tasks.")
+  .version("1.0.0")
+  .description("My Node CLI")
+  .option("-n, --name <type>", "Add your name")
+  .action((options) => {
+    console.log(`Hey, ${options.name}!`);
+    console.log("I love it");
+    console.log("I love it very much");
+    console.log("I love it very much");
+  });
 
-program
-  .command('say-hello')
-  .description('Say hello to the world')
-  .action(() => {
-    console.log('Hello world!');
-  })
+program.parse(process.argv);

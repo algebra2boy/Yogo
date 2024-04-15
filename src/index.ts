ade7ac13dc38f2047577680116835175005e03f3
Yongye Tan <yongyetan1209@gmail.com>
2024-04-15 18:06:01 +0000

    add cool ascii character

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import figlet from "figlet";
+import chalk from "chalk";
+
 import { openSite } from "./commands/openSite/openSite.js";
 import { contribution } from "./commands/contributions/checkContribution.js";
 
 const program = new Command();
+
+// cool ASCII character
+console.log(
+  chalk.blueBright(figlet.textSync("YOGO", {
+    font: "3D-ASCII",
+    width: 100,
+    whitespaceBreak: true
+  }))
+);
 
 program
   .name("yogo")
